627ddd5489ec74d2050dd05d45f80a7aa3a3558a
Peter Nagy <peternagyxx@gmail.com>
2016-06-12 15:56:47 +0200

    refactor checkInput

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ var ERRORS = {
 var ROMAN_DIGITS = ["I", "V", "X", "L", "C", "D", "M"]
 
 var checkInput = function (number) {
+
+
+  throw new Error(ERRORS.INVALID_VALUE)
+}
+
+var checkArabicInput = function(number) {
+  if (typeof  number === "number") {
+    //check number range
+    if (number < 1 || number > 3999) throw new Error(ERRORS.INVALID_RANGE)
+    return number
+  }
+  return null
+}
+
+var checkRomanInput = function(number) {
   if (typeof  number === "string") {
     //empty string not allowed
     if (number.length === 0) throw new Error(ERRORS.INVALID_VALUE)
@@ -15,31 +30,26 @@ var checkInput = function (number) {
       // more than 3 repeating digit is not allowed
       if ((i > 2) && (number[i] === number[i - 1]) && (number[i - 2] === number[i - 1]) && (number[i - 3] === number[i - 2])) throw new Error(ERRORS.INVALID_VALUE)
     }
-    return
+    return number
   }
-  if (typeof  number === "number") {
-    //check number range
-    if (number < 1 || number > 3999) throw new Error(ERRORS.INVALID_RANGE)
-    return
-  }
-  throw new Error(ERRORS.INVALID_VALUE)
+  return null
 }
 
 
 function RomanNumber (number) {
 
-  checkInput(number)
-  this.int = null
-  this.string = null
+  if (!number) throw new Error(ERRORS.INVALID_VALUE)
+  this.arabic = checkArabicInput(number)
+  this.string = checkRomanInput(number)
 
 }
 
 RomanNumber.prototype.toInt = function() {
-  return null
+  if (this.arabic !== null) return this.arabic
 }
 
 RomanNumber.prototype.toString = function() {
-  return null
+  if (this.roman !== null) return this.roman
 }
 
 function testRoman () {
@@ -47,7 +57,7 @@ function testRoman () {
   var number, tested, error, expected
 
   tested = 0
-  error = ERRORS.INVALID_RANGE
+  error = ERRORS.INVALID_VALUE
   try {
     var isEx = false
     number = new RomanNumber(tested)
